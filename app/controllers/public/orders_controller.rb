0f2d191ac2cfa6a8de4335f610c48a21046109a0
ryokatakahashi <ryonryon.xxxx@gmail.com>
2023-10-19 14:47:44 +0000

    [UPDATE]会員側の注文に関するページ作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -58,62 +58,62 @@ class Public::OrdersController < ApplicationController
     # 各カートアイテムの価格と数量の金額を格納するのに使用
     ary = []
     # 商品の価格×数量＝金額(ary配列に追加)
-    @cart_items.each do |cart_item|
-      ary << cart_item.item.price*cart_item.quantity
-    end
-    # ary.sumですべてのカートアイテムの金額を計算し、＠cart_items_priceに設定
-    @cart_items_price = ary.sum
-    #送料とカートアイテムの合計金額
-    @order.totle_price = @order.postage + @cart_items_price
-    # ユーザーが選択した支払い方法を設定
-    @order.payment_method = params[:order][:payment_method]
-    if @order.payment_method == "credit_card"
-      @order.status = 0 #クレジットカード
-    else
-      @order.status = 1 #銀行振込
-    end
+      @cart_items.each do |cart_item|
+        ary << cart_item.item.price*cart_item.quantity
+      end
+      # ary.sumですべてのカートアイテムの金額を計算し、＠cart_items_priceに設定
+      @cart_items_price = ary.sum
+      #送料とカートアイテムの合計金額
+      @order.total_payment = @order.postage + @cart_items_price
+      # ユーザーが選択した支払い方法を設定
+      @order.payment_method = params[:order][:payment_method]
+      if @order.payment_method == "credit_card"
+        @order.status = 0 #クレジットカード
+      else
+        @order.status = 1 #銀行振込
+      end
 
-    address_type = params[:order][:address_type]
-    #address_typeに応じて異なる配送先情報を@oederに設定
+      address_type = params[:order][:address_type]
+      #address_typeに応じて異なる配送先情報を@oederに設定
     case address_type
-  when "customer_address" #カスタマーの登録住所を@orderに設定
-    @order.post_code = current_customer.post_code
-    @order.address = current_customer.address
-    @order.name = current_customer.last_name + current_customer.first_name
-  #フォームから送信されたregistered_address_idを使用し、登録済み住所情報をselectedに取得し、@orderに設定
-  when "registered_address"
-    Address.find(params[:order][:registered_address_id])
-    selected = Address.find(params[:order][:registered_address_id])
-    @order.post_code = selected.post_code
-    @order.address = selected.address
-    @order.name = selected.name
-  #フォームから送信された新しい住所情報を@orderに設定
-  when "new_address"
-    @order.post_code = params[:order][:new_post_code]
-    @order.address = params[:order][:new_address]
-    @order.name = params[:order][:new_name]
-  end
-
-  if @order.save #成功したら
-    if @order.status == 0 #製造ステータスが未着手である
-      @cart_items.each do |cart_item|
-        OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 0)
-      end
-    else #０以外の場合→製造開始である
-      @cart_items.each do |cart_item|
-        OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 1)
-      end
+    when "customer_address" #カスタマーの登録住所を@orderに設定
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    #フォームから送信されたregistered_address_idを使用し、登録済み住所情報をselectedに取得し、@orderに設定
+    when "registered_address"
+      Address.find(params[:order][:registered_address_id])
+      selected = Address.find(params[:order][:registered_address_id])
+      @order.post_code = selected.post_code
+      @order.address = selected.address
+      @order.name = selected.name
+    #フォームから送信された新しい住所情報を@orderに設定
+    when "new_address"
+      @order.post_code = params[:order][:new_post_code]
+      @order.address = params[:order][:new_address]
+      @order.name = params[:order][:new_name]
     end
-    # 注文処理後、カート内を全て削除
-    @cart_items.destroy_all
-    # 注文正常処理後、注文完了を知らせる
-    redirect_to complete_orders_path 
-  else #注文処理失敗
-    # 他のviewを表示しエラーメッセージを表示
-    render :items
+
+    if @order.save #成功したら
+      if @order.status == 0 #製造ステータスが未着手である
+        @cart_items.each do |cart_item|
+          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 0)
+        end
+      else #０以外の場合→製造開始である
+        @cart_items.each do |cart_item|
+          OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 1)
+        end
+      end
+      # 注文処理後、カート内を全て削除
+      @cart_items.destroy_all
+      # 注文正常処理後、注文完了を知らせる
+      redirect_to complete_orders_path 
+    else #注文処理失敗
+      # 他のviewを表示しエラーメッセージを表示
+      render :items
+    end
   end
-  end
-  end
+  
 
   def complete
   end
@@ -131,4 +131,5 @@ class Public::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: @order.id)
   end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@ class Order < ApplicationRecord
 
 #payment_methodへenumを定義
 enum payment_method: { credit_card: 0, transfer: 1 }
-
+  
 
 end
