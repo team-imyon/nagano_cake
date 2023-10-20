ef33a6ead91137c9cd98eaff2db7a8f519330108
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-10-20 10:47:47 +0000

    パス変更等

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,5 +8,19 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :furigana_last_name, :furigana_first_name, :post_code, :address, :tel_number, :email, :is_active)
+  end
+end  
 end
