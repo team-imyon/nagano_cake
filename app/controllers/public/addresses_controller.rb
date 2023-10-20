804720cda10ab5c1748239b0e79c3694927516ac
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-10-20 10:47:55 +0000

    ページの追加・修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,13 +1,48 @@
 class Public::AddressesController < ApplicationController
   def index
-    @address = Address.find(params[:id])
-    @addressn = Address.new
+    @customer = current_customer
+    @address = @customer.addresses
+    @naddress = Address.new
+  end
+  
+  def create
+    @naddress = Address.new(address_params)
+    @naddress.customer_id = current_customer.id
+    if @naddress.save
+      redirect_to addresses_path
+    else
+      @customer = current_customer
+      @address = @customer.addresses
+      @naddress = Address.new
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    address = Address.find(params[:id])
+    if address.update(address_params)
+      redirect_to addresses_path
+    else
+      @address = Address.find(params[:id])
+      render :edit
+    end
+  end
+  
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+  
+  private
+  
+    def address_params
+    params.require(:address).permit(:post_code, :address, :name)
+
   end
 end
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,7 @@ class Address < ApplicationRecord
   belongs_to :customer
   # 配送先が繋がっているモデルはcustomerのみ
   
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 end
