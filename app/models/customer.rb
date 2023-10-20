423ef8c29e3851130d7e06d3a5a6254657eeb8e2
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-10-20 02:45:47 +0000

    Add アソシエーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,14 @@ class Customer < ApplicationRecord
   
   def full_name
     self.last_name+" "+self.first_name
-    
+    # ↑注文履歴とかでフルネームを表示する方法
+
   end
+  
+  has_many :addresses, dependent: :destroy
+  # 会員は配送先を沢山持っている
+  has_many :orders, dependent: :destroy
+  # 会員は注文を沢山持っている
+  has_many :cart_items, dependent: :destroy
+  # 会員はカートアイテムを沢山持っている
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,6 @@
 class Genre < ApplicationRecord
+  
+  has_many :items, dependent: :destroy
+  # 商品ジャンルは沢山の商品情報を持っている。
+  
 end
