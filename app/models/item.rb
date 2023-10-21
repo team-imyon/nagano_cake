a7567468305e6af5ff9ceb28565e9fb1ac779aca
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-10-21 10:54:17 +0000

    顧客商品一覧ページ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,35 +1,34 @@
 class Item < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
   has_many :cart_items, dependent: :destroy
   # 商品情報は沢山のcart_itemsを持っている。
-  
+
   has_many :order_details, dependent: :destroy
   # 商品情報は沢山のorder_details(注文履歴の商品)を持っている。
-  
+
   belongs_to :genre
   # 商品情報に繋がっているのはgenreのみ
-  
+
   has_one_attached :image
   # 画像投稿のためにimageをもたせる
-  
+
   ## 消費税を求めるメソッド
   def with_tax_price
       (price * 1.1).floor
-  end  
-  
+  end
+
   ## 小計を求めるメソッド
   def subtotal
       item.with_tax_price * amount
   end
-  
-  def get_image
+
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/cake.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [100, 100]).processed
+    image.variant(resize_to_limit: [width, height]).processed
   end
-  
 end
