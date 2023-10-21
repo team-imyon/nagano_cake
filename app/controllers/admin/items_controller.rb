f5bbcf8e38b40e1754bce54d9435b3bc33f50ba6
kk-um <143879591+kk-um@users.noreply.github.com>
2023-10-21 08:45:13 +0000

    admin 商品新規登録/編集画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,29 +1,36 @@
 class Admin::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
-    
+
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
-    item = @item
-    item.save
-    redirect_to 'admin/items/:id'
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)  
   end
 
   def show
   end
-  
+
   private
   # ストロングパラメータ
-  def list_params
-    params.require(:item).permit(:image, :name, :explanation, :price)
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :genre_id, :price, :is_active)
   end
-  
+
 end
