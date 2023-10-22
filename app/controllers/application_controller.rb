feadfaac4fc3a0d70d7a57253dd708a7d09760b7
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-10-22 05:36:43 +0000

    Add ログイン後遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_items_path
+    when Customer
+      items_path
+    end
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
+  
   protected
-
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:furigana_last_name,:furigana_first_name,:post_code,:address,:tel_number])
