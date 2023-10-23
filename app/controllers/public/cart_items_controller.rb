d80c46bcd6ea9af4fb34a69d150cc856e14bd9c0
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-10-23 01:02:13 +0000

    カートupdate

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items
-    @total = 0
   end
 
   def update
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,5 +5,10 @@ class CartItem < ApplicationRecord
 
   belongs_to :item
 # カート内商品が繋がっているモデルはitemのみ
+
+  # 小計を求めるメソッド
+  def subtotal
+      item.with_tax_price * amount
+  end
   
 end
