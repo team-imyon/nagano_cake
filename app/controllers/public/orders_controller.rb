9bc8e6ef99cbe8159c783b407f1964778e6f651a
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-10-24 10:20:23 +0000

    カート＆その他update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -48,7 +48,7 @@ class Public::OrdersController < ApplicationController
       end
     end
   end
-  
+
   def create
     #ryon
     # byebug
@@ -100,9 +100,9 @@ class Public::OrdersController < ApplicationController
       @order.name = params[:order][:new_name]
     end
 
-    if @order.save 
+    if @order.save
     #saveが成功したら
-      if @order.status == 0 
+      if @order.status == 0
       #製造ステータスが未着手である
         @cart_items.each do |cart_item|
           OrderDetail.create!(order_id: @order.id, item_id: cart_item.item.id, price: cart_item.item.price, amount: cart_item.amount, making_status: 0)
