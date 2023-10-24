00eee1763632e2475841843ea6ff3e586b2547e4
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-10-24 10:22:52 +0000

    Updateロゴ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,8 +1,5 @@
 class Admin::OrdersController < ApplicationController
   
-  def index
-  end
-  
   def show
     @order = Order.find(params[:id]).order(created_at: :desc)
     @order_details = OrderDetail.where(order_id: @order.id)
