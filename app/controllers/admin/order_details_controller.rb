dc9974845eda67af68d04ae45f4665a7a4075ea0
Maki-koko <144005937+Maki-koko@users.noreply.github.com>
2023-10-26 10:38:46 +0000

    Update調整

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -6,42 +6,38 @@ class Admin::OrderDetailsController < ApplicationController
     @order_details = @order.order_details.all
     @order_detail.update(order_detail_params)
     
-    if @order_detail.making_status == "completed"
-      @order_detail.update(order_detail_params)
+    is_updated = true
+      if @order_detail.update(order_detail_params)
+        @order.update(status: "production") if @order_detail.making_status == "production"
         @order_details.each do |order_detail|
-        if order_detail.making_status = "production"
-          redirect_to admin_order_path(@order)
-        else
-          @order.status = 3
-          @order.update(order_params)
+          if order_detail.making_status != "completed"
+            is_updated = false
+          end
         end
+        @order.update(status: "preparing") if is_updated
       end
-    end
-    redirect_to admin_order_path(@order)
+        redirect_to admin_order_path(@order)
   end
 
-    # is_updated = true
-    #   if @order_detail.update(order_detail_params)
-    #     @order.update(status: "production") if @order_detail.making_status == "production"
-    #     @order_details.each do |order_detail|
-    #       if order_detail.making_status != "completed"
-    #         is_updated = false
-    #       end
-    #     end
-    #     @order.update(status: "preparing") if is_updated
-    #   end
-    # redirect_to admin_order_path(@order)
-  # end
-    # @order.update(order_params)
-    # if @order.status == "confirmation"
-    #   @order_details.each do |order_detail|
-    #     order_detail.making_status = 1
-    #     order_detail.update(order_detail_params)
-    #   end
-    # end
-    #   redirect_to admin_order_path(@order)
-    
-
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details.all
+    @order_detail.update(order_detail_params)
+  
+      if @order_detail.making_status == "completed"
+        @order_detail.update(order_detail_params)
+          @order_details.each do |order_detail|
+            if order_detail.making_status = "production"
+            redirect_to admin_order_path(@order)
+            else
+            @order.status = 3
+            @order.update(order_params)
+            end
+          end
+      end
+    redirect_to admin_order_path(@order)
+  end  
   
 private
  def order_detail_params
@@ -51,6 +47,5 @@ private
   def order_params
     params.permit(:status)
   end
-
   
 end
